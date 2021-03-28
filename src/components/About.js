7f5c23545ee86855f035cc33499d317878ddc26a
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2021-03-28 18:25:22 +0200

    refactorovan about.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import services from "../constants/services";
-//import Title from "./Title";
 import Title from "./Title2";
-import kutija from "../images/kutija.png";
 
 const About = () => {
   return (
@@ -13,121 +11,56 @@ const About = () => {
         subtitle="Šta mi radimo"
         title="Sve što Vam je potrebno za savršen poklon"
       ></Title>
-      <div className="services">
+      <div className="section-center">
         {services.map(({ id, icon, label, text }) => {
           return (
-            <div key={id} className="service">
+            <article key={id}>
               <div className="service-header">
                 <span>{icon}</span>
                 <h4>{label}</h4>
               </div>
               <p>{text}</p>
-            </div>
+            </article>
           );
         })}
-        <div className="about-us-img-wrapper">
-          <img src={kutija} />
-        </div>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
-  background: var(--clr-white);
-  .services {
-    margin-top: 6rem;
-    width: 100%;
-    height: 100%;
+  .section-center {
+    max-width: 900px;
+    margin: 4rem auto;
     display: grid;
-    grid-row-gap: 2rem;
-    .about-us-img-wrapper {
-      display: none;
+    gap: 3rem;
+    /* safari workaround */
+    grid-gap: 3rem;
+    article {
     }
-    .service {
+    .service-header {
       display: flex;
-      flex-direction: column;
       align-items: center;
       justify-content: center;
-      width: 90%;
-      margin: 0 auto;
+      margin-bottom: 1rem;
 
-      .service-header {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+      .icon {
+        font-size: 4rem;
+        color: var(--clr-primary-5);
         margin-bottom: 1rem;
-        .icon {
-          font-size: 4rem;
-          color: var(--clr-primary-5);
-          margin-right: 1rem;
-        }
-        h4 {
-          text-transform: uppercase;
-          font-weight: 500;
-          font-size: 1.4rem;
-        }
+        margin-right: 1rem;
       }
-      p {
-        text-align: center;
-        color: var(--clr-black);
-        max-width: 35em;
-        font-size: 1.2rem;
+      h4 {
+        text-transform: uppercase;
+        font-weight: 500;
       }
     }
-    @media (min-width: 868px) {
-      grid-template-columns: repeat(16, 1fr);
-      grid-template-rows: repeat(4, 6rem);
-      grid-row-gap: 4rem;
-      .service {
-        width: 100%;
-        align-items: flex-start;
-        p {
-          text-align: left;
-        }
-      }
-      .service:nth-child(1) {
-        grid-column: 3 / 6;
-        grid-row: 1 / 3;
-      }
-
-      .service:nth-child(2) {
-        grid-column: 3 / 6;
-        grid-row: 3 / 5;
-      }
-
-      .service:nth-child(3) {
-        grid-column: 12/ 15;
-        grid-row: 3 / 5;
-      }
-
-      .service:nth-child(4) {
-        grid-column: 12 / 15;
-        grid-row: 1 / 3;
-      }
-
-      .service:nth-child(5) {
-        grid-column: 12 / 15;
-        grid-row: 3 / 5;
-      }
-
-      .service:nth-child(6) {
-        grid-column: 11 / 14;
-        grid-row: 5 / -1;
-      }
-      .about-us-img-wrapper {
-        display: block;
-        grid-column: 7 / 11;
-        grid-row: 2 / 6;
-        width: 100%;
-      }
-
-      .about-us-img-wrapper img {
-        width: 100%;
-        transform: scale(2);
-        object-fit: cover;
-        opacity: 1;
-      }
+    p {
+      margin: 0 auto;
+      max-width: 35em;
+    }
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
     }
   }
 `;
