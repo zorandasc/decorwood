ce065c8e023856f20fc8e00020108377cd8a7331
Zoran Dasic <zorandsc@yahoo.com>
2021-03-28 01:33:42 +0100

    contact finito

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,14 +6,19 @@ import formImg from "../images/form-img.png";
 
 const Contact = () => {
   return (
-    <Wrapper id="contact">
-      <h1 className="section-heading">Contact</h1>
+    <Wrapper className="section" id="contact">
+      <Title
+        subtitle="Pišite name"
+        title="Da zajedno kreiramo nove projekte"
+      ></Title>
 
       <form className="contact-form">
         <input type="text" placeholder="Your Name" />
         <input type="email" placeholder="Your Email" />
         <textarea placeholder="Your Message"></textarea>
-        <input type="submit" value="Send" />
+        <button className="submit" type="submit">
+          Send
+        </button>
       </form>
     </Wrapper>
   );
@@ -27,21 +32,32 @@ const Wrapper = styled.section`
   align-items: center;
 
   .contact-form {
+    margin-top: 3rem;
+    margin-bottom: 5rem;
     width: 45rem;
+    background-color: #fff;
     display: flex;
     flex-direction: column;
-    padding: 1rem;
+    padding: 3rem;
     border-radius: 0.5rem;
+    box-shadow: 3rem 3rem 4rem #aaa;
     transition: all 0.5s;
+  }
+
+  .contact-form:hover {
+    transform: translateY(-2rem);
+    box-shadow: 5rem 5rem 7rem #aaa;
   }
 
   .contact-form input,
   .contact-form textarea {
-    outline: none;
+    margin: 1rem;
+    padding: 1rem;
     font-size: 1.3rem;
     letter-spacing: 0.1rem;
     border: none;
-    border-radius: 2rem;
+    box-shadow: inset 0.1rem 0.1rem 0.2rem #ddd,
+      inset -0.1rem -0.1rem 0.2rem #ddd;
   }
 
   .contact-form textarea {
@@ -51,16 +67,24 @@ const Wrapper = styled.section`
     min-width: 70%;
   }
 
-  .contact-form input[type="submit"] {
-    color: #fff;
-    font-weight: bolder;
-    letter-spacing: 0.5rem;
+  .submit {
+    width: 8rem;
+    margin-top: 1rem;
+    display: inline-block;
+    padding: 0.5rem 1rem;
+    background: var(--clr-primary-5);
+    text-transform: capitalize;
+    letter-spacing: 5px;
+    font-size: 1rem;
+    border-radius: 3rem;
+    border: 2px solid var(--clr-primary-5);
+    color: var(--clr-white);
     cursor: pointer;
-    transition: background-color 0.5s;
   }
 
-  .contact-form input[type="submit"]:hover {
-    background-color: #32e7ff;
+  .submit:hover {
+    background-color: var(--clr-white);
+    color: var(--clr-primary-5);
   }
 `;
 
